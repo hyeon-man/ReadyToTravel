12faac1712979c43a7e59d1292b8932c91ceada5
hsm <hhpp1205@naver.com>
2023-03-28 12:00:30 +0900

    first commit

---
+++ src/main/java/kr/ac/kopo/ReadyToTravel/ReadyToTravelApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ReadyToTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadyToTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadyToTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/kopo/ReadyToTravel/ReadyToTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ReadyToTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadyToTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
