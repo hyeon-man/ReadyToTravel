0c4c8e86454ad5939a905956a3ede6a060b6dbab
hyeoonnm <rkdwogus77@gmail.com>
2023-03-29 13:13:14 +0900

    지도에 마커 표시하면 날씨 값 html에 표시하는 기능

--- src/main/java/kr/ac/kopo/ReadyToTravel/api/weather/WeatherController.java
+++ src/main/java/kr/ac/kopo/ReadyToTravel/api/weather/WeatherController.java
@@ -1,4 +1,31 @@
 package kr.ac.kopo.ReadyToTravel.api.weather;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Controller
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/weather")
 public class WeatherController {
+
+    @PostMapping("/changeAddress")
+    public ResponseEntity<AddressChanger> changeAddress(@RequestBody String splitResult) throws Exception {
+        AddressParse parse = new AddressParse();
+
+        String substring = splitResult.substring(1, splitResult.length()-1);
+
+        AddressChanger changer = parse.changer(substring);
+
+        return ResponseEntity.ok().body(changer);
+    }
 }
--- src/main/java/kr/ac/kopo/ReadyToTravel/root/RootController.java
+++ src/main/java/kr/ac/kopo/ReadyToTravel/root/RootController.java
@@ -1,4 +1,12 @@
 package kr.ac.kopo.ReadyToTravel.root;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class RootController {
+
+    @RequestMapping("/test")
+    public String weather() {
+        return "tmapV2";
+    }
 }
