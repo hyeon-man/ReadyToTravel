82036898a7f5e0610bae20d1d2f61a846bb14015
hyeon-man <twingfull@gmail.com>
2023-05-31 20:06:07 +0900

    0531     보드 어태치 다시해야함     inform css 퍼블리싱

--- src/main/java/kr/ac/kopo/ReadyToTravel/board/BoardAttachCustomRepository.java
+++ src/main/java/kr/ac/kopo/ReadyToTravel/board/BoardAttachCustomRepository.java
@@ -1,7 +1,7 @@
-package kr.ac.kopo.ReadyToTravel.board;
-
-import java.util.List;
-
-public interface BoardAttachCustomRepository {
-    List<String> findByFileNameByBoardNum(Long boardNum);
-}
+//package kr.ac.kopo.ReadyToTravel.board;
+//
+//import java.util.List;
+//
+//public interface BoardAttachCustomRepository {
+//    List<String> findByFileNameByBoardNum(Long boardNum);
+//}
