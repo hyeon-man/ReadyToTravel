cd38080a3b577bf50db40d50f4928a318ad39d4e
hyeon-man <twingfull@gmail.com>
2023-06-08 10:22:41 +0900

    06-07   GroupRemoveScheduler 작성중   패키징 변경

--- src/main/java/kr/ac/kopo/ReadyToTravel/member/MemberController.java
+++ src/main/java/kr/ac/kopo/ReadyToTravel/member/MemberController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
