2b34f4a15349e854ee1069db645f821d0fd30b66
ShinKangWon <ekdrms5153@naver.com>
2023-06-14 18:43:44 +0900

    6/14 profile 게시글 리스트 파싱작업 수행중

--- src/main/resources/static/js/member/profile.js
+++ src/main/resources/static/js/member/profile.js
@@ -86,7 +86,96 @@ const eyeIcons = document.querySelectorAll('.eyes ion-icon');
 eyeIcons.forEach(icon => {
     icon.addEventListener('click', togglePassword);
 });
+//모달 그룹페이지
+document.addEventListener("DOMContentLoaded", function() {
+    var liElement = document.querySelector(".group_page");
+    var modal = document.getElementById("modal3");
+    var closeButton = document.querySelector(".close-button-group");
 
+    liElement.addEventListener("click", function() {
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden"; // Disable scrolling on the body
+
+        // 구성원을 동적으로 추가하는 함수
+        function addMember(member) {
+            var membersContainer = document.querySelector(".members");
+
+            var memberDiv = document.createElement("div");
+            memberDiv.className = "member";
+
+            var memberImage = document.createElement("img");
+            memberImage.src = member.profileIMG;
+            memberImage.alt = "member";
+            memberDiv.appendChild(memberImage);
+
+            var memberInfoDiv = document.createElement("div");
+            memberInfoDiv.style.flex = "1";
+
+            var memberId = document.createElement("p");
+            memberId.textContent = member.memberId;
+            memberInfoDiv.appendChild(memberId);
+
+            var memberName = document.createElement("p");
+            memberName.textContent = member.name;
+            memberInfoDiv.appendChild(memberName);
+
+            var memberEmail = document.createElement("p");
+            memberEmail.textContent = member.email;
+            memberInfoDiv.appendChild(memberEmail);
+
+            var memberPhoneNum = document.createElement("p");
+            memberPhoneNum.textContent = member.phoneNum;
+            memberInfoDiv.appendChild(memberPhoneNum);
+
+            memberDiv.appendChild(memberInfoDiv);
+
+            membersContainer.appendChild(memberDiv);
+        }
+
+        // 서버에서 JSON 데이터를 받아와 구성원 정보를 동적으로 추가하는 함수
+        function fetchGroupData() {
+            fetch("/member/profile/groupData")
+                .then(response => response.json())
+                .then(data => {
+                    var titleElement = document.querySelector(".modal-content3 h3");
+                    var contentsElement = document.querySelector(".modal3-1-text");
+                    var members = data.memberDTO;
+
+                    titleElement.textContent = data.name;
+                    contentsElement.textContent = data.contents;
+
+                    for (var i = 0; i < members.length; i++) {
+                        addMember(members[i]);
+                    }
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+        }
+
+        // 그룹 데이터를 동적으로 추가
+        fetchGroupData();
+    });
+
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto"; // Enable scrolling on the body
+    });
+});
+
+//모달 그룹페이지 안에 more button
+const moreButton = document.querySelector('.more-button');
+const modal = document.getElementById('modal3-1');
+
+moreButton.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+modal.addEventListener('click', function(e) {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
 // ------------------------------------이미지 비동기 변경 출처G쌤 매핑해서해보셈-----------------------
 
 // 파일 선택 이벤트 리스너 등록
